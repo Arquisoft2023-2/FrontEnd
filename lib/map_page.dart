e6c64406ebbc1c4a9da540cf6ce476ddd56c3bb3
jyarac <jyarac@unal.edu.co>
2023-10-14 13:46:49 -0500

    Adelanto en la peticion de notificaciones

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quickalert/quickalert.dart';
 import 'dart:async';
 import './services/storage_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 //create blank page with scaffold called MapPage, the page should not have a back button
 class MapPage extends StatelessWidget {
@@ -11,10 +13,26 @@ class MapPage extends StatelessWidget {
 
   final notificationUrl = "http://localhost:1000/apiNotification";
 
-  void recurrentQuery() async {
+  void recurrentQuery(BuildContext context) async {
     Timer.periodic(Duration(minutes: 1), (timer) async {
       String? token = await SecureStorage().readSecureData("token");
-      //por ahora solo se envia el token
+      String? plate = await SecureStorage().readSecureData("Plate");
+      String graphQLQuery = ' { getNotifications(id: "$plate"){ message } }';
+
+      try {
+        var response = await http.post(Uri.parse(notificationUrl),
+            headers: {"Content-type": "application/json", "token": token!},
+            body: json.encode({'query': graphQLQuery}));
+        if (response.statusCode == 200) {
+          var data = jsonDecode(response.body);
+          print(data);
+          if (data["data"]["getNotifications"].length > 0) {
+            showAlert(context);
+          }
+        }
+      } catch (e) {
+        print(e);
+      }
     });
   }
 
