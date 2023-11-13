d16f474d7a5ceccc2c1cbb0b5436acd33cdebce0
jyarac <jyarac@unal.edu.co>
2023-11-13 13:44:30 -0500

    Creacion peticion soap

--- lib/user_info.dart
+++ lib/user_info.dart
@@ -7,134 +7,164 @@ import 'components/textfield.dart';
 import 'map_page.dart';
 import './services/storage_item.dart';
 
+//soap request to get flight information
+//create function called soapRequest
+Future soapRequest() async {
+  //create envelope
+  String envelope =
+      '''<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <tem:GetFlightsRequest></tem:GetFlightsRequest>
+   </soapenv:Body>
+</soapenv:Envelope>''';
 
+  //create headers
+  Map<String, String> headers = {
+    "Content-type": "text/xml; charset=utf-8",
+    "SOAPAction": "http://localhost:8000/wsdl",
+    "Host": "localhost:8000"
+  };
+
+  //make the request
+  http.Response response = await http.post(
+      Uri.parse("http://localhost:8000/wsdl"),
+      headers: headers,
+      body: envelope);
+  print(response.body);
+  print("hello, soap petition working");
+  return response.body;
+}
+
+//user info page
 class UserInfo extends StatelessWidget {
- 
-
   //controladores de edición de texto
   final userIDController = TextEditingController();
   final passwordController = TextEditingController();
 
   final loginUrl = "http://localhost:1000/apiUser";
 
-  //controladores boton de inicio de sesion  
-  getData() async{
+  //call function soapRequest and save the result in var result
+  var result = soapRequest();
+  //controladores boton de inicio de sesion
+  getData() async {
     String? name = await SecureStorage().readSecureData("name");
     String? age = await SecureStorage().readSecureData("age");
     String? license = await SecureStorage().readSecureData("license");
     String? plate = await SecureStorage().readSecureData("plate");
-    Map map ={
+    Map map = {
       "name": name,
       "age": age,
-      "license": license ,
-      "plate": plate ,
-
+      "license": license,
+      "plate": plate,
     };
-    return map; 
+    return map;
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: MenuLateral(),
         appBar: AppBar(
-        title: const Text("Información de usuario"),
-        backgroundColor: const LinearGradient(
-          colors: [Color(0xFF7CC6CD), Color(0XFF001F29)],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ).colors[0],
-      ),
-        body: 
-        FutureBuilder(future: getData(),
-        
-        builder: (BuildContext context, AsyncSnapshot snap) {
-          if(snap.connectionState == ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator());
-          }
-          else{
-            return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SizedBox(height: 15, width: 2000,),
-              SizedBox(height: 150, width: 150, child: CircleAvatar(
-                child: ClipOval(
-                  child: Image.network('https://cdn-icons-png.flaticon.com/512/1995/1995504.png',
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.cover,
-                  ),
-                ),
-              ),),
-              
-              SizedBox(height: 30),
-              Text(
-                "Avión tripulado:",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                snap.data["plate"] ?? "Default",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Nombre:",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                 snap.data["name"] ?? "Default",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 15),
-               Text(
-                "Licencia:",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                snap.data["license"] ?? "Default",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Edad:",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                snap.data["age"] ?? "Defaultd",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 15),
-              
-            ]);
-        }
-          }
-          
-        
-        ,)
-        
-    );
+          title: const Text("Información de usuario"),
+          backgroundColor: const LinearGradient(
+            colors: [Color(0xFF7CC6CD), Color(0XFF001F29)],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ).colors[0],
+        ),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot snap) {
+            if (snap.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 15,
+                      width: 2000,
+                    ),
+                    SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: CircleAvatar(
+                        child: ClipOval(
+                          child: Image.network(
+                            'https://cdn-icons-png.flaticon.com/512/1995/1995504.png',
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      "Avión tripulado:",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      snap.data["plate"] ?? "Default",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "Nombre:",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      snap.data["name"] ?? "Default",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "Licencia:",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      snap.data["license"] ?? "Default",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "Edad:",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      snap.data["age"] ?? "Defaultd",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                  ]);
+            }
+          },
+        ));
   }
 }
 /*
